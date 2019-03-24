531b55e04aab98af146fad820643937ad4a34492
rongpan <rongpan17@gmail.com>
2019-03-24 02:08:11 -0500

    update cegis scripts

--- shells/runTutor2cegis.sh
+++ shells/runTutor2cegis.sh
@@ -2,7 +2,7 @@
 
 t="timeout 300"
 output_folder="results1/tutor2cegis/"
-for filename in tests/clean_AutoTutor/*; do
+for filename in tests/clean_AutoTutorWithTrue/*; do
   echo $filename
   echo $output_folder${filename##*/}
   $t java -jar target/regfixer.jar --mode 2 -c -t fix --file $filename > $output_folder${filename##*/}
